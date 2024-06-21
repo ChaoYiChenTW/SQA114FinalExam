a949356ce1a6ffe6a27300d1b6b4e64359d2acf5
ChaoYiChen <a951159aca@gmail.com>
2024-06-20 20:27:58 -0700

    update test

--- tests/test.js
+++ tests/test.js
@@ -2,36 +2,36 @@ const { By, Key, Builder } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 
 async function test_case() {
-
-    //Set Chrome option
+    // Set Chrome options
     let options = new chrome.Options();
     options.addArguments('headless');
-    options.addArguments('disable-gpu')
+    options.addArguments('disable-gpu');
     options.setChromeBinaryPath('/usr/bin/google-chrome');
 
-    // Create a Driver
+    // Create a driver
     let driver = await new Builder().forBrowser("chrome").setChromeOptions(options).build();
 
     try {
-        //Send driver to website
+        // Send driver to website
         await driver.get("http://3.81.20.62/");
         console.log('Website opened');
-        driver.sleep(1000);
+        await driver.sleep(1000);
 
-        let playButton = driver.findElement(By.id('okBtn'));
-        playButton.click();
+        let playButton = await driver.findElement(By.id('okBtn'));
+        await playButton.click();
         console.log('Play button clicked');
 
         // Find and click on a cell
-        let cell = driver.findElement(By.id('cell0'));
-        cell.click();
+        let cell = await driver.findElement(By.id('cell0'));
+        await cell.click();
         console.log('Cell clicked');
 
         // Wait for a brief moment to ensure the update is reflected (adjust as necessary)
-        driver.sleep(1000);
+        await driver.sleep(1000);
 
         // Check the content of the cell after click
-        let cellContent = cell.getAttribute('innerHTML');
+        let cellContent = await cell.getAttribute('innerHTML');
+        console.log('Cell content:', cellContent);
 
         // Check if it shows "<span class=\"x\">&times;</span>"
         if (cellContent.trim() === '<span class="x">&times;</span>') {
@@ -40,10 +40,10 @@ async function test_case() {
             console.log('Test Failed');
         }
     } catch (error) {
-        console.log('An error accurred:', error);
+        console.log('An error occurred:', error);
     } finally {
         await driver.quit();
     }
-
 }
+
 test_case();
