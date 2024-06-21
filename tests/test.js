f578821021109550b7a4b2fd181ac5fb693f4433
ChaoYiChen <a951159aca@gmail.com>
2024-06-20 20:22:44 -0700

    update test

--- tests/test.js
+++ tests/test.js
@@ -16,21 +16,22 @@ async function test_case() {
         //Send driver to website
         await driver.get("http://3.81.20.62/");
         console.log('Website opened');
+        driver.sleep(1000);
 
-        let playButton = await driver.findElement(By.id('okBtn'));
-        await playButton.click();
+        let playButton = driver.findElement(By.id('okBtn'));
+        playButton.click();
         console.log('Play button clicked');
 
         // Find and click on a cell
-        let cell = await driver.findElement(By.id('cell0'));
-        await cell.click();
+        let cell = driver.findElement(By.id('cell0'));
+        cell.click();
         console.log('Cell clicked');
 
         // Wait for a brief moment to ensure the update is reflected (adjust as necessary)
-        await driver.sleep(1000);
+        driver.sleep(1000);
 
         // Check the content of the cell after click
-        let cellContent = await cell.getAttribute('innerHTML');
+        let cellContent = cell.getAttribute('innerHTML');
 
         // Check if it shows "<span class=\"x\">&times;</span>"
         if (cellContent.trim() === '<span class="x">&times;</span>') {
